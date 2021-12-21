98c6d39a55c8b9195f36298e1d3cd236133b4345
Rayane Ventura <rayyventura@gmail.com>
2021-12-21 19:42:17 -0300

    Fixing detail with link

--- src/components/ChoseSeats/index.js
+++ src/components/ChoseSeats/index.js
@@ -90,7 +90,7 @@ export default function ChoseSeats({ setInfos, info }) {
     } else {
       setErrorMessage(false);
     }
-    navigate('/successo');
+    navigate('/sucesso');
   }
 
   return (
